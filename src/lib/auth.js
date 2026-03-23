f67bbf493e587d3891abb92c64f9f6ceed73cb0f
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-03-23 10:49:59 +0300

    create better auth correct form

---
+++ src/lib/auth.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "../generated/prisma/client";
+
+
+const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, { provider: "postgresql" }),
+
+    emailAndPassword: {
+        enabled: true,
+    },
+
+    socialProviders: {
+        google: { clientId: "", clientSecret: "" },
+        github: { clientId: "", clientSecret: "" }
+    }
+});
