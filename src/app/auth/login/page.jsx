afd8e87fdb3ad0f627d8eed00dfdfb6b41bb811d
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-03-28 22:00:21 +0300

    update ui

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -3,12 +3,13 @@ import LoginForm from "./Form";
 export default function LoginPage() {
   return (
     <div
-      className="min-h-screen flex items-center justify-center bg-slate-50 dark:bg-slate-900"
+      className="min-h-screen flex items-center justify-center"
       style={{
         backgroundImage:
-          "url('https://i.pinimg.com/1200x/9f/86/f1/9f86f195892b6f3b797f8a7ad13ffff6.jpg')",
+          "url('https://images.pexels.com/photos/281260/pexels-photo-281260.jpeg')",
         backgroundSize: "cover",
         backgroundPosition: "center",
+        backgroundAttachment: "fixed",
       }}
     >
       <LoginForm />
